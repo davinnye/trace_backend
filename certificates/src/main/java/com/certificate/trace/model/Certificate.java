3ed3fab50dff84502b36eec71d24a94c3c36c1a1
davinnye <viniciusferreira0708@gmail.com>
2024-09-25 13:16:53 -0300

    feat: last updates for v1 of trace-certificates-backend

--- certificates/src/main/java/com/certificate/trace/model/Certificate.java
+++ certificates/src/main/java/com/certificate/trace/model/Certificate.java
@@ -11,7 +11,7 @@ import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity(name = "Certificate")
+@Entity(name = "certificates")
 @Getter
 @Setter
 public class Certificate {
